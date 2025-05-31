b43f79f0c36c3c7e475c01d889c05c9fdd946c5f
mlakar-marinela <marinelamlakar03@gmail.com>
2025-06-01 01:14:06 +0300

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -1,44 +1,58 @@
 import { useState } from 'react';
 import './App.css'
 
+let index = 0;
+
 function App() {
-const [tasks, setTasks] = useState(["Task 1", "Task2", "Task3"]);
+  const [tasks, setTasks] = useState(["Task 1", "Task 2", "Task 3"]);
+
+  // Allow drop
   const allowDrop = (e) => {
     e.preventDefault();
   };
-  
+
+  // Identify the <li> that you want to drag
   const dragStart = (e) => {
-    e.dataTransfer.setData("text/plain", e.target.id);
+    e.dataTransfer.setData("text/plain", e.currentTarget.id);
   };
 
+  // Drop the <li> that you identified above in the <ul>
   const handleDrop = (e) => {
     e.preventDefault();
     const data = e.dataTransfer.getData("text/plain");
     const draggedElement = document.getElementById(data);
-    const target = e.target;
-    if(target.tagName === 'UL') {
-      target.appendChild(draggedElement);
+    let dropTarget = e.target;
+    while (dropTarget && dropTarget.tagName !== 'UL') {
+      dropTarget = dropTarget.parentElement;
     }
-  };
+    if (dropTarget && draggedElement) {
+      dropTarget.appendChild(draggedElement);
+    }
+  }
 
+// Alow the <li> to be contentEditable
   const activateContentEditing = (e) => {
     e.currentTarget.contentEditable = 'true';
     e.currentTarget.focus();
   }
 
-  const stopContentEditing = (e, index) => {
-    const updatedTasks = [...tasks];
-    updatedTasks[index] = e.currentTarget.innerText;
-    setTasks(updatedTasks);
+// Update the state of the content from the <li> element and stop the content editing
+  const stopContentEditing = (e) => {
+    const index = e.currentTarget.getAttribute("data-task-index");
+    if (index !== null) {
+      const updatedTasks = [...tasks];
+      updatedTasks[parseInt(index)] = e.currentTarget.innerText;
+      setTasks(updatedTasks);
+    }
     e.currentTarget.contentEditable = 'false';
   }
 
-  let index = 0;
-
+// Generate a new <li> element
   const generateListItem = (boxClass) => {
     const newListItem = document.createElement('li');
+    const uniqueId = 'new' + index++;
     newListItem.classList.add('newListItem');
-    newListItem.id = 'new' + index++;
+    newListItem.id = uniqueId;
     newListItem.setAttribute('contentEditable', 'false');
     newListItem.setAttribute('draggable', 'true');
     newListItem.addEventListener('dragstart', dragStart);
